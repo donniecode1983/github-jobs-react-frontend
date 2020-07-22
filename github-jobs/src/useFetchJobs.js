bb0651f8cc48290ffa6fd198b2171b1c4bf00219
Donovan J. Murphy <=39827226+donniecode1983@users.noreply.github.com>
2020-07-22 16:13:55 -0500

    Added Cancel Token feature

--- github-jobs/src/useFetchJobs.js
+++ github-jobs/src/useFetchJobs.js
@@ -1,4 +1,4 @@
-import {useReducer, useEffect} from 'react';
+import {useReducer, useEffect} from 'react'
 import axios from 'axios'
 
 const ACTIONS = {
@@ -25,14 +25,21 @@ const reducer = (state, action) => {
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, {jobs:[], loading: true})
     useEffect(() => {
+        const cancelToken = axios.CancelToken.source()
         dispatch({type: ACTIONS.MAKE_REQUEST})
         axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
             params: {markdown: true, page: page, ...params}
         }).then(res =>  {
             dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
         }).catch(e => {
+            if (axios.isCancel(e)) return
             dispatch({type: ACTIONS.ERROR, payload: {error: e}})
         })
+
+        return () => {
+            cancelToken.cancel()
+        }
 
     }, [params, page])
     return state
