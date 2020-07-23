12ed0b798fbcd117f46315c806e77ca1926aed34
Donovan J. Murphy <=39827226+donniecode1983@users.noreply.github.com>
2020-07-23 10:34:56 -0500

    Added the useStates and job.map

--- github-jobs/src/App.js
+++ github-jobs/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useFetchJobs from './useFetchJobs';
 import {Container, Row, Col} from 'react-bootstrap';
+import Job from './Job'
 
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
-  const {jobs, loading, error} = useFetchJobs()
+  const [params, setParams] = useState({})
+    const [page, setPage] = useState(1)
+  const {jobs, loading, error} = useFetchJobs(params, page)
   return (
       <Container>
           <Row>
               <Col>
                   {loading && <h1>Loading...</h1>}
                   {error && <h1>Error, Try Refreshing</h1>}
-                  <h1>{jobs.length}</h1>
+                  {jobs.map(job => {
+                      return <Job key={job.id} job={job} />
+                  })}
               </Col>
           </Row>
       </Container>
