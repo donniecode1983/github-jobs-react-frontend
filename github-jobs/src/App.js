9c8e10714cc3efbdfe58dc3408c921bdcc127a3b
Donovan J. Murphy <=39827226+donniecode1983@users.noreply.github.com>
2020-07-23 11:06:23 -0500

    Removed the Row and Col components & added an H1 header

--- github-jobs/src/App.js
+++ github-jobs/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import useFetchJobs from './useFetchJobs';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import Job from './Job'
 
 // import logo from './logo.svg';
@@ -11,16 +11,13 @@ function App() {
     const [page, setPage] = useState(1)
   const {jobs, loading, error} = useFetchJobs(params, page)
   return (
-      <Container>
-          <Row>
-              <Col>
+      <Container className={"my-4"}>
+          <h1>GitHub Jobs</h1>
                   {loading && <h1>Loading...</h1>}
                   {error && <h1>Error, Try Refreshing</h1>}
                   {jobs.map(job => {
                       return <Job key={job.id} job={job} />
                   })}
-              </Col>
-          </Row>
       </Container>
   );
 }
