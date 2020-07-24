733d20cee0fca9dfc97bf19bf9cab944d74f5f3d
Donovan J. Murphy <=39827226+donniecode1983@users.noreply.github.com>
2020-07-24 12:25:51 -0500

    Added updates to integrate the Job Pagination Component

--- github-jobs/src/useFetchJobs.js
+++ github-jobs/src/useFetchJobs.js
@@ -4,11 +4,13 @@ import axios from 'axios'
 const ACTIONS = {
     MAKE_REQUEST: 'make-request',
     GET_DATA: 'get-data',
-    ERROR: 'error'
+    ERROR: 'error',
+    UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 // const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
 const BASE_URL = 'http://localhost:3000/data.json'
+
 const reducer = (state, action) => {
     switch (action.type) {
         case ACTIONS.MAKE_REQUEST:
@@ -17,6 +19,8 @@ const reducer = (state, action) => {
             return {...state, loading:false, jobs: action.payload.jobs}
         case ACTIONS.ERROR:
             return {...state, loading:false, error: action.payload.error, jobs: []}
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return {...state, hasNextPage: action.payload.hasNextPage}
         default:
             return state;
     }
@@ -25,10 +29,10 @@ const reducer = (state, action) => {
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, {jobs:[], loading: true})
     useEffect(() => {
-        const cancelToken = axios.CancelToken.source()
+        const cancelToken1 = axios.CancelToken.source()
         dispatch({type: ACTIONS.MAKE_REQUEST})
         axios.get(BASE_URL, {
-            cancelToken: cancelToken.token,
+            cancelToken: cancelToken1.token,
             params: {markdown: true, page: page, ...params}
         }).then(res =>  {
             dispatch({type: ACTIONS.GET_DATA, payload: {jobs: res.data}})
@@ -37,8 +41,21 @@ export default function useFetchJobs(params, page) {
             dispatch({type: ACTIONS.ERROR, payload: {error: e}})
         })
 
+        //Check if there is a Next Page.
+        const cancelToken2 = axios.CancelToken.source()
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken2.token,
+            params: {markdown: true, page: page + 1, ...params}
+        }).then(res =>  {
+            dispatch({type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: {hasNextPage: res.data.length != 0}})
+        }).catch(e => {
+            if (axios.isCancel(e)) return
+            dispatch({type: ACTIONS.ERROR, payload: {error: e}})
+        })
+
         return () => {
-            cancelToken.cancel()
+            cancelToken1.cancel()
+            cancelToken2.cancel()
         }
 
     }, [params, page])
